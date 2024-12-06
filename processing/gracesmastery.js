bcadaeaf698f0307958eaf5ea8e9ce0704ae8189
voldo <filipecavoldo@outlook.com>
2024-12-06 17:21:44 -0300

    moved processing and related files to another folder

---
+++ processing/gracesmastery.js
@@ -0,0 +1,759 @@
+/* eslint-disable */
+module.exports = {
+  "nimble fang": [
+    "90",
+    "450"
+  ],
+  "dual impact": [
+    "80",
+    "400"
+  ],
+  "burial blade": [
+    "70",
+    "350"
+  ],
+  "stagger strike": [
+    "90",
+    "450"
+  ],
+  "wing crush": [
+    "90",
+    "450"
+  ],
+  "gush stroke": [
+    "90",
+    "450"
+  ],
+  "bestial roar": [
+    "80",
+    "400"
+  ],
+  "wolfwind fang": [
+    "80",
+    "400"
+  ],
+  "phantom pulse": [
+    "80",
+    "400"
+  ],
+  "dragon charge": [
+    "70",
+    "350"
+  ],
+  "heaven's wrath": [
+    "70",
+    "350"
+  ],
+  "void sword": [
+    "70",
+    "350"
+  ],
+  "demon fist": [
+    "90",
+    "450"
+  ],
+  "icedragon flurry": [
+    "80",
+    "400"
+  ],
+  "lightning strike": [
+    "100",
+    "800"
+  ],
+  "demon fang": [
+    "100",
+    "800"
+  ],
+  "shockwave slash": [
+    "100",
+    "800"
+  ],
+  "frost breaker": [
+    "100",
+    "800"
+  ],
+  "moonlight circle": [
+    "100",
+    "800"
+  ],
+  "ruinous bloom": [
+    "100",
+    "800"
+  ],
+  "phantasm flash": [
+    "100",
+    "800"
+  ],
+  "concussive bolt": [
+    "100",
+    "800"
+  ],
+  "gale maw": [
+    "100",
+    "800"
+  ],
+  "abyssal seal": [
+    "90",
+    "720"
+  ],
+  "infernal torrent": [
+    "90",
+    "720"
+  ],
+  "plasma shock": [
+    "90",
+    "720"
+  ],
+  "sparrow seal": [
+    "90",
+    "720"
+  ],
+  "diabolic rage": [
+    "90",
+    "720"
+  ],
+  "incineration wave": [
+    "80",
+    "640"
+  ],
+  "aurora lotus": [
+    "70",
+    "560"
+  ],
+  "shattersteel swarm": [
+    "80",
+    "640"
+  ],
+  "triple strike": [
+    "90",
+    "450"
+  ],
+  "double moon": [
+    "80",
+    "400"
+  ],
+  "piercing blast": [
+    "70",
+    "350"
+  ],
+  "eagle dive": [
+    "90",
+    "450"
+  ],
+  "elegant flash": [
+    "90",
+    "450"
+  ],
+  "dragon dash": [
+    "90",
+    "450"
+  ],
+  "demolition drive": [
+    "80",
+    "400"
+  ],
+  "lucent palisade": [
+    "80",
+    "400"
+  ],
+  "phantom dance": [
+    "80",
+    "400"
+  ],
+  "spirit rush": [
+    "70",
+    "350"
+  ],
+  "sundering fury": [
+    "70",
+    "350"
+  ],
+  "angelic ascent": [
+    "70",
+    "350"
+  ],
+  "reaper's toll": [
+    "90",
+    "450"
+  ],
+  "beast": [
+    "80",
+    "400"
+  ],
+  "deadly force": [
+    "70",
+    "350"
+  ],
+  "first aid": [
+    "40",
+    "320"
+  ],
+  "heal": [
+    "35",
+    "280"
+  ],
+  "cure": [
+    "30",
+    "240"
+  ],
+  "rejuvenate": [
+    "25",
+    "200"
+  ],
+  "ataraxia": [
+    "90",
+    "720"
+  ],
+  "sensor flare": [
+    "90",
+    "720"
+  ],
+  "rapier rush": [
+    "100",
+    "800"
+  ],
+  "sword assault": [
+    "100",
+    "800"
+  ],
+  "dualblade spin": [
+    "90",
+    "720"
+  ],
+  "dagger splash": [
+    "100",
+    "800"
+  ],
+  "bladerang hurl": [
+    "100",
+    "800"
+  ],
+  "shotstaff blast": [
+    "90",
+    "720"
+  ],
+  "gauntlet blitz": [
+    "70",
+    "560"
+  ],
+  "slash rose": [
+    "80",
+    "640"
+  ],
+  "invigoration": [
+    "25",
+    "200"
+  ],
+  "new moon": [
+    "90",
+    "450"
+  ],
+  "full moon": [
+    "80",
+    "400"
+  ],
+  "falling star": [
+    "70",
+    "350"
+  ],
+  "tiger blade": [
+    "90",
+    "450"
+  ],
+  "gale fang": [
+    "90",
+    "450"
+  ],
+  "thunder fang": [
+    "90",
+    "450"
+  ],
+  "detonation bloom": [
+    "80",
+    "400"
+  ],
+  "dual shredder": [
+    "80",
+    "400"
+  ],
+  "whirlwind slash": [
+    "80",
+    "400"
+  ],
+  "mega sonic thrust": [
+    "70",
+    "350"
+  ],
+  "dualblade dance": [
+    "70",
+    "350"
+  ],
+  "thunder bringer": [
+    "70",
+    "350"
+  ],
+  "vital flair": [
+    "80",
+    "400"
+  ],
+  "flare shot": [
+    "100",
+    "800"
+  ],
+  "aqua bullet": [
+    "100",
+    "800"
+  ],
+  "scarlet salvo": [
+    "100",
+    "800"
+  ],
+  "radial arc": [
+    "100",
+    "800"
+  ],
+  "aerial barrage": [
+    "100",
+    "800"
+  ],
+  "covering fire": [
+    "100",
+    "800"
+  ],
+  "gravity blast": [
+    "90",
+    "720"
+  ],
+  "cross mirage": [
+    "90",
+    "720"
+  ],
+  "beast blaster": [
+    "80",
+    "640"
+  ],
+  "absolute zero": [
+    "70",
+    "560"
+  ],
+  "antidote": [
+    "40",
+    "320"
+  ],
+  "healing wind": [
+    "35",
+    "280"
+  ],
+  "rolling thunderbolt": [
+    "90",
+    "720"
+  ],
+  "blessed blades": [
+    "50",
+    "250"
+  ],
+  "steel baptism": [
+    "40",
+    "200"
+  ],
+  "majestic salvo": [
+    "30",
+    "150"
+  ],
+  "serpent tooth": [
+    "50",
+    "250"
+  ],
+  "scathing harvest": [
+    "50",
+    "250"
+  ],
+  "spectral purge": [
+    "40",
+    "200"
+  ],
+  "thunder surge": [
+    "40",
+    "200"
+  ],
+  "infinite wave": [
+    "30",
+    "150"
+  ],
+  "frozen rain": [
+    "30",
+    "150"
+  ],
+  "soul brigade": [
+    "30",
+    "150"
+  ],
+  "pixie circle": [
+    "40",
+    "320"
+  ],
+  "healing circle": [
+    "40",
+    "320"
+  ],
+  "fairy circle": [
+    "35",
+    "280"
+  ],
+  "nurse": [
+    "30",
+    "240"
+  ],
+  "revitalize": [
+    "25",
+    "200"
+  ],
+  "resurrection": [
+    "25",
+    "200"
+  ],
+  "remedy": [
+    "40",
+    "320"
+  ],
+  "recover": [
+    "30",
+    "240"
+  ],
+  "insight": [
+    "35",
+    "280"
+  ],
+  "fleetfoot": [
+    "35",
+    "280"
+  ],
+  "veil of faith": [
+    "35",
+    "280"
+  ],
+  "seraphic bolt": [
+    "40",
+    "320"
+  ],
+  "ethereal rift": [
+    "40",
+    "320"
+  ],
+  "lightning blast": [
+    "35",
+    "280"
+  ],
+  "divine saber": [
+    "30",
+    "240"
+  ],
+  "virtuous radiance": [
+    "25",
+    "200"
+  ],
+  "indignation": [
+    "25",
+    "200"
+  ],
+  "second wind": [
+    "50",
+    "250"
+  ],
+  "third degree": [
+    "40",
+    "200"
+  ],
+  "fourth circle": [
+    "30",
+    "150"
+  ],
+  "rolling flare": [
+    "50",
+    "250"
+  ],
+  "darkness veil": [
+    "50",
+    "250"
+  ],
+  "corruption claw": [
+    "40",
+    "200"
+  ],
+  "magna resolver": [
+    "40",
+    "200"
+  ],
+  "sonic mount": [
+    "30",
+    "150"
+  ],
+  "ground breaker": [
+    "30",
+    "150"
+  ],
+  "rock lance": [
+    "40",
+    "320"
+  ],
+  "cluster raid": [
+    "40",
+    "320"
+  ],
+  "air pressure": [
+    "35",
+    "280"
+  ],
+  "disruption": [
+    "30",
+    "240"
+  ],
+  "photon burst": [
+    "40",
+    "320"
+  ],
+  "burning strike": [
+    "40",
+    "320"
+  ],
+  "phantom flare": [
+    "35",
+    "280"
+  ],
+  "blazing hearts": [
+    "30",
+    "240"
+  ],
+  "cold front": [
+    "40",
+    "320"
+  ],
+  "ice spires": [
+    "40",
+    "320"
+  ],
+  "glacial rain": [
+    "35",
+    "280"
+  ],
+  "frigid coffin": [
+    "30",
+    "240"
+  ],
+  "spectral chains": [
+    "40",
+    "320"
+  ],
+  "deathmire": [
+    "35",
+    "280"
+  ],
+  "culling flare": [
+    "30",
+    "240"
+  ],
+  "dragon slayer": [
+    "25",
+    "200"
+  ],
+  "eleth flare": [
+    "30",
+    "240"
+  ],
+  "last resort": [
+    "25",
+    "200"
+  ],
+  "emission line": [
+    "50",
+    "250"
+  ],
+  "star scatter": [
+    "40",
+    "200"
+  ],
+  "petal burst": [
+    "30",
+    "150"
+  ],
+  "freezing bloom": [
+    "50",
+    "250"
+  ],
+  "nighthawk talon": [
+    "50",
+    "250"
+  ],
+  "vicious circle": [
+    "40",
+    "200"
+  ],
+  "sudden impact": [
+    "40",
+    "200"
+  ],
+  "star crusher": [
+    "30",
+    "150"
+  ],
+  "death sentence": [
+    "30",
+    "150"
+  ],
+  "flux form": [
+    "40",
+    "200"
+  ],
+  "wind needle": [
+    "40",
+    "320"
+  ],
+  "fearful storm": [
+    "35",
+    "280"
+  ],
+  "grim sylphie": [
+    "30",
+    "240"
+  ],
+  "pyrogenic ring": [
+    "40",
+    "320"
+  ],
+  "infernal prison": [
+    "35",
+    "280"
+  ],
+  "bloodflamme": [
+    "30",
+    "240"
+  ],
+  "spread": [
+    "40",
+    "320"
+  ],
+  "maelstrom": [
+    "35",
+    "280"
+  ],
+  "cyandine": [
+    "30",
+    "240"
+  ],
+  "trinity bit": [
+    "40",
+    "320"
+  ],
+  "assault sign": [
+    "35",
+    "280"
+  ],
+  "rampart sign": [
+    "35",
+    "280"
+  ],
+  "shining field": [
+    "30",
+    "240"
+  ],
+  "enflame": [
+    "90",
+    "720"
+  ],
+  "magnetic gate": [
+    "40",
+    "320"
+  ],
+  "fangirl dream": [
+    "25",
+    "200"
+  ],
+  "celestial eight": [
+    "25",
+    "200"
+  ],
+  "poor form": [
+    "50",
+    "250"
+  ],
+  "harsh rebuttal": [
+    "40",
+    "200"
+  ],
+  "unbridled scorn": [
+    "30",
+    "150"
+  ],
+  "lightning blade": [
+    "50",
+    "250"
+  ],
+  "hurricane thrust": [
+    "50",
+    "250"
+  ],
+  "sword rain: alpha": [
+    "40",
+    "200"
+  ],
+  "severing wind": [
+    "40",
+    "200"
+  ],
+  "fiery beast": [
+    "30",
+    "150"
+  ],
+  "rising phoenix": [
+    "30",
+    "150"
+  ],
+  "moonlight circle pupil": [
+    "40",
+    "200"
+  ],
+  "double moon pupil": [
+    "40",
+    "200"
+  ],
+  "soul reaper": [
+    "40",
+    "320"
+  ],
+  "sword deluge": [
+    "40",
+    "320"
+  ],
+  "shade imperial": [
+    "35",
+    "280"
+  ],
+  "vengeant fangs": [
+    "40",
+    "320"
+  ],
+  "antidote pupil": [
+    "40",
+    "320"
+  ],
+  "lightning blast pupil": [
+    "35",
+    "280"
+  ],
+  "deathmire pupil": [
+    "35",
+    "280"
+  ],
+  "infernal prison pupil": [
+    "35",
+    "280"
+  ],
+  "holy lance": [
+    "30",
+    "240"
+  ],
+  "shooting stars": [
+    "25",
+    "200"
+  ]
+}
